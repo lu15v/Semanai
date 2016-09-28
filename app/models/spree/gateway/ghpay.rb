66976fea85409b28a24bc24896d663080a191f8a
Fernando Gomez <gomezhyuuga@gmail.com>
2016-09-28 17:19:33 -0500

    FIX gateway model

--- app/models/spree/gateway/ghpay.rb
+++ app/models/spree/gateway/ghpay.rb
@@ -1,17 +1,65 @@
-class Spree::Gateway::Ghpay < Spree::Gateway
-  def provider_class
-    Spree::Gateway::Ghpay
-  end
-  def payment_source_class
-    Spree::CreditCard
-  end
+module Spree
+  class Gateway::Ghpay < Gateway
+    attr_accessor :test
 
-  def method_type
-    'foopay'
-  end
+    def provider_class
+      self.class
+    end
 
-  def purchase(amount, transaction_details, options = {})
-    # Always accept the payment
-    ActiveMerchant::Billing::Response.new(true, 'success', {}, {})
+    def preferences
+      {}
+    end
+
+    def create_profile(payment)
+      # simulate the storage of credit card profile using remote service
+      success = true
+      payment.source.update_attributes(:gateway_customer_profile_id => generate_profile_id(success))
+    end
+
+    def authorize(money, credit_card, options = {})
+      ActiveMerchant::Billing::Response.new(true, 'Bogus Gateway: Forced success', {}, :test => true, :authorization => '12345', :avs_result => { :code => 'A' })
+    end
+
+    def purchase(money, credit_card, options = {})
+      profile_id = credit_card.gateway_customer_profile_id
+      ActiveMerchant::Billing::Response.new(true, 'Bogus Gateway: Forced success', {}, :test => true, :authorization => '12345', :avs_result => { :code => 'A' })
+    end
+
+    def credit(money, credit_card, response_code, options = {})
+      ActiveMerchant::Billing::Response.new(true, 'Bogus Gateway: Forced success', {}, :test => true, :authorization => '12345')
+    end
+
+    def capture(authorization, credit_card, gateway_options)
+      ActiveMerchant::Billing::Response.new(true, 'Bogus Gateway: Forced success', {}, :test => true, :authorization => '67890')
+    end
+
+    def void(response_code, credit_card, options = {})
+      ActiveMerchant::Billing::Response.new(true, 'Bogus Gateway: Forced success', {}, :test => true, :authorization => '12345')
+    end
+
+    def test?
+      # Test mode is not really relevant with bogus gateway (no such thing as live server)
+      true
+    end
+
+    def payment_profiles_supported?
+      true
+    end
+
+    def actions
+      %w(capture void credit)
+    end
+
+    private
+    def generate_profile_id(success)
+      record = true
+      prefix = success ? 'BGS' : 'FAIL'
+      while record
+        random = "#{prefix}-#{Array.new(6){rand(6)}.join}"
+        record = CreditCard.where(:gateway_customer_profile_id => random).first
+      end
+      random
+    end
   end
 end
+
--- db/migrate/20160928220456_create_gh_gateway.rb
+++ db/migrate/20160928220456_create_gh_gateway.rb
@@ -1,10 +1,5 @@
 class CreateGhGateway < ActiveRecord::Migration
   def up
-    Spree::Gateway::Ghpay.create(
-      name: 'GHPAY',
-      description: 'Custom Gateway for iweek',
-      active: true,
-      preferences: { server: "test", test_mode: true })
   end
   def down
   end
